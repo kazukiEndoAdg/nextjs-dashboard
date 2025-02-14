ec1034294dfe03353f0cf1c6f7ec17ae5cf35253
kazukiEndoAdg <kazuki.endo@adglobe.co.jp>
2025-02-14 16:43:54 +0900

    add: feature of streaming a component

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,15 +5,15 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 // NOTE: async server component
 // The page is an async server component. This allows you to use await to fetch data
  // このページは非同期サーバーコンポーネントです。これにより、データをフェッチするためにawaitを使用することができます。
 export default async function Page() {
-    const latestInvoices = await fetchLatestInvoices();
+    // const latestInvoices = await fetchLatestInvoices();
     const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
   
   return (
@@ -32,11 +32,14 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        {/* Note: Streaming a component
+        Wrapping a specific component allow you to have granular control of loading skelton. */}
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart  />
         </Suspense>
-        
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
